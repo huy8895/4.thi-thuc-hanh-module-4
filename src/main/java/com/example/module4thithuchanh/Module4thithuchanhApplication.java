468a53b6719a5fe16dafa61ba36a91963bf351a6
huy8895 <huy8895@gmail.com>
2020-10-13 09:23:23 +0700

    first commit

---
+++ src/main/java/com/example/module4thithuchanh/Module4thithuchanhApplication.java
@@ -0,0 +1,13 @@
+package com.example.module4thithuchanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4thithuchanhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module4thithuchanhApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/module4thithuchanh/Module4thithuchanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module4thithuchanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4thithuchanhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
